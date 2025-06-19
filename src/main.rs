e6852502d745bfd0ed0928acadf787c2687744cc
Tunahan Dundar <tdundar@onepin.com>
2025-06-19 12:25:13 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 #![allow(unused_imports)]
-use core::panic;
+use core::{num, panic};
 use std::collections::HashMap;
-use std::io::{Read, Write};
+use std::fs::File;
+use std::io::{BufRead, BufReader, Error, Read, Seek, Write};
 use std::iter::Peekable;
 use std::net::{TcpListener, TcpStream};
+use std::path::Path;
 use std::str::{self, Chars};
 use std::sync::{Arc, Mutex};
-use std::time::{Duration, Instant};
+use std::time::{Duration, Instant, SystemTime, UNIX_EPOCH};
 use std::{env, thread};
 
 #[derive(Eq, Hash, PartialEq, Clone)]
@@ -19,6 +21,148 @@ enum RespDataType {
     Nil,
     Boolean(bool),
     // Double(f32),
+}
+
+struct Db {}
+impl Db {
+    fn system_time_to_instant(&mut self, sys_time: SystemTime) -> Instant {
+        let now_sys = SystemTime::now();
+        let now_inst = Instant::now();
+
+        match sys_time.duration_since(now_sys) {
+            // sys_time is in the future ⇒ add the delta
+            Ok(delta) => now_inst + delta,
+            // sys_time is in the past ⇒ subtract the delta
+            Err(err) => now_inst - err.duration(),
+        }
+    }
+
+    fn size_encoded_bytes(&mut self, reader: &mut BufReader<File>) -> u32 {
+        let mut number_byte = [0u8; 1];
+        reader.read_exact(&mut number_byte).unwrap();
+        let byte = number_byte[0];
+        match byte & 0xC0 {
+            0 => {
+                return byte.try_into().unwrap();
+            }
+            1 => {
+                let mut next_byte = [0u8; 1];
+                reader.read_exact(&mut next_byte).unwrap();
+                return (2u8.pow(8) * byte & 0x3F + next_byte[0])
+                    .try_into()
+                    .unwrap();
+            }
+            2 => {
+                let mut next_four_bytes = [0u8; 4];
+                reader.read_exact(&mut next_four_bytes).unwrap();
+                return u32::from_le_bytes(next_four_bytes);
+            }
+            3 => match byte {
+                0xC0 => {
+                    let mut next_one_byte = [0u8; 1];
+                    reader.read_exact(&mut next_one_byte).unwrap();
+                    return next_one_byte[0].try_into().unwrap();
+                }
+                0xC1 => {
+                    let mut next_two_bytes = [0u8; 2];
+                    reader.read_exact(&mut next_two_bytes).unwrap();
+                    return u16::from_le_bytes(next_two_bytes).into();
+                }
+                0xC2 => {
+                    let mut next_four_bytes = [0u8; 4];
+                    reader.read_exact(&mut next_four_bytes).unwrap();
+                    return u32::from_le_bytes(next_four_bytes);
+                }
+                _ => {}
+            },
+            _ => {
+                unreachable!("should not be more than 3")
+            }
+        }
+        return 1;
+    }
+
+    fn string_encoded(&mut self, reader: &mut BufReader<File>) -> String {
+        let string_length = self.size_encoded_bytes(reader);
+        let mut string_buffer: Vec<u8> = vec![0u8; string_length as usize];
+        reader.read_exact(&mut string_buffer).unwrap();
+        return String::from_utf8(string_buffer).unwrap();
+    }
+
+    fn load(&mut self, state: State) -> Result<(), Error> {
+        if let (Some(db_dir), Some(db_file)) = (state.db_dir, state.db_file_name) {
+            let file = File::open(&Path::new(format!("{}/{}", db_dir, db_file).as_str()))?;
+            let mut reader = BufReader::new(file);
+            let mut _discard = Vec::new();
+            reader.read_until(0xFE, &mut _discard).unwrap();
+            let _db_index = self.size_encoded_bytes(&mut reader);
+            reader.consume(1);
+            let keys_size = self.size_encoded_bytes(&mut reader);
+            let _keys_size_with_expiration_size = self.size_encoded_bytes(&mut reader);
+            for _ in 0..keys_size {
+                let mut first_byte = [0u8; 1];
+                reader.read_exact(&mut first_byte).unwrap();
+                let raw_byte = first_byte[0];
+                match raw_byte {
+                    0x00 => {
+                        let hash_key = self.string_encoded(&mut reader);
+                        let hash_value = self.string_encoded(&mut reader);
+                        println!("{} {}", hash_key, hash_value);
+                        let mut state_guard = state.shared_data.lock().unwrap();
+                        state_guard.insert(
+                            RespDataType::BulkString(hash_key),
+                            ExpiringValue {
+                                value: RespDataType::BulkString(hash_value),
+                                expiration_timestamp: None,
+                            },
+                        );
+                    }
+                    0xFC => {
+                        let mut expiration_as_miliseconds_bytes = [0u8; 8];
+                        reader
+                            .read_exact(&mut expiration_as_miliseconds_bytes)
+                            .unwrap();
+                        let ms = u64::from_le_bytes(expiration_as_miliseconds_bytes);
+                        reader.consume(1);
+                        let hash_key = self.string_encoded(&mut reader);
+                        let hash_value = self.string_encoded(&mut reader);
+                        let expiration_time = UNIX_EPOCH + Duration::from_millis(ms);
+                        let mut state_guard = state.shared_data.lock().unwrap();
+                        state_guard.insert(
+                            RespDataType::BulkString(hash_key),
+                            ExpiringValue {
+                                value: RespDataType::BulkString(hash_value),
+                                expiration_timestamp: Some(Instant::from(
+                                    self.system_time_to_instant(expiration_time),
+                                )),
+                            },
+                        );
+                    }
+                    0xFD => {
+                        let mut expiration_as_seconds_bytes = [0u8; 4];
+                        reader.read_exact(&mut expiration_as_seconds_bytes).unwrap();
+                        let seconds = u32::from_le_bytes(expiration_as_seconds_bytes);
+                        reader.consume(1);
+                        let hash_key = self.string_encoded(&mut reader);
+                        let hash_value = self.string_encoded(&mut reader);
+                        let expiration_time = UNIX_EPOCH + Duration::from_secs(seconds.into());
+                        let mut state_guard = state.shared_data.lock().unwrap();
+                        state_guard.insert(
+                            RespDataType::BulkString(hash_key),
+                            ExpiringValue {
+                                value: RespDataType::BulkString(hash_value),
+                                expiration_timestamp: Some(Instant::from(
+                                    self.system_time_to_instant(expiration_time),
+                                )),
+                            },
+                        );
+                    }
+                    _ => {}
+                }
+            }
+        }
+        return Ok(());
+    }
 }
 
 struct Parser {}
@@ -106,6 +250,9 @@ impl CommandExecutor {
                         }
                     }
                 }
+                "KEYS" => {
+                    self.keys_command(commands, stream, state);
+                }
                 _ => {}
             },
             _ => {}
@@ -177,9 +324,7 @@ impl CommandExecutor {
         let (key, value) = (commands[1].clone(), commands[2].clone());
         let mut hashmap_value = ExpiringValue {
             value,
-            created_at: Instant::now(),
-            last_accessed: Instant::now(),
-            expiration_as_miliseconds: None,
+            expiration_timestamp: None,
         };
         let mut options: Option<&[RespDataType]> = None;
         if commands.len() > 3 {
@@ -195,8 +340,10 @@ impl CommandExecutor {
                     let RespDataType::BulkString(option_value) = &op[1] else {
                         unreachable!("protocol invariant violated: expected BulkString");
                     };
-                    hashmap_value.expiration_as_miliseconds =
-                        Some(option_value.parse::<u128>().unwrap());
+                    hashmap_value.expiration_timestamp = Some(
+                        Instant::now()
+                            + Duration::from_millis(option_value.parse::<u64>().unwrap()),
+                    );
                 }
                 _ => {}
             }
@@ -217,8 +364,8 @@ impl CommandExecutor {
         let value = state_guard.get(&key);
         match value {
             Some(v) => {
-                if let Some(exp) = v.expiration_as_miliseconds {
-                    if v.created_at.elapsed().as_millis() > exp {
+                if let Some(expiration_timestamp) = v.expiration_timestamp {
+                    if Instant::now() > expiration_timestamp {
                         return stream.write_all(b"$-1\r\n").unwrap();
                     }
                 }
@@ -229,6 +376,31 @@ impl CommandExecutor {
                 return stream.write_all(bulk_response.as_bytes()).unwrap();
             }
             None => stream.write_all(b"$-1\r\n").unwrap(),
+        }
+    }
+
+    fn keys_command(
+        &mut self,
+        commands: &mut Vec<RespDataType>,
+        stream: &mut TcpStream,
+        state: State,
+    ) {
+        let shared_guard = state.shared_data.lock().unwrap();
+        if let RespDataType::BulkString(regex_match) = &commands[1] {
+            let mut matching_keys: Vec<String> = Vec::new();
+            for key in shared_guard.keys() {
+                if let RespDataType::BulkString(key) = key {
+                    let parts: Vec<&str> = regex_match.split("*").collect();
+                    if key.starts_with(parts[0]) && key.ends_with(parts[1]) {
+                        matching_keys.push(key.clone());
+                    }
+                }
+            }
+            let mut result = String::from(format!("*{}\r\n", matching_keys.len()));
+            for mk in matching_keys {
+                result.push_str(&self.convert_bulk_string_to_resp(&mk));
+            }
+            stream.write_all(result.as_bytes()).unwrap();
         }
     }
 
@@ -245,9 +417,7 @@ impl CommandExecutor {
 
 struct ExpiringValue {
     value: RespDataType,
-    created_at: Instant,
-    last_accessed: Instant,
-    expiration_as_miliseconds: Option<u128>,
+    expiration_timestamp: Option<Instant>,
 }
 
 #[derive(Clone)]
@@ -273,6 +443,7 @@ fn main() {
     if args.len() > 1 {
         state.db_dir = Some(args[2].clone());
         state.db_file_name = Some(args[4].clone());
+        let _ = Db {}.load(state.clone());
     }
 
     fn handle_connection(stream: &mut TcpStream, state: State) {
